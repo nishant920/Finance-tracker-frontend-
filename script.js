a226d5739a4eb557c96127808759e117e3186f8e
nishant920 <nishantdhiman044@gmail.com>
2026-07-26 00:57:19 +0530

    Update API_BASE_URL to http://16.192.93.16:8080/api

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // --------------------------------------------------------------------------
 
 // Backend API Base URL configuration - change this easily for deployment
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "http://16.192.93.16:8080/api";
 
 /**
  * Retrieve the JWT token stored in browser localStorage
